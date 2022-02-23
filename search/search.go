fee14226c85aeb039baba5fe660448338cb3230d
ErdemOzgen <m.erdemozgen@gmail.com>
2022-02-23 23:10:03 +0300

    add SearchByAuthor in search package

--- search/search.go
+++ search/search.go
@@ -1,1 +1,21 @@
 package search
+
+import (
+	"fmt"
+	"homework-1-ErdemOzgen/model"
+	"homework-1-ErdemOzgen/utils"
+)
+
+type Book model.Book
+
+func SearchByAuthor(author string, books []Book) {
+	fmt.Println("Searching...") // TODO: After location set get rid of this line
+	for _, book := range books {
+		if book.Author == author {
+			fmt.Println("Founded") // TODO: After location set get rid of this line
+			//fmt.Println(book)
+			utils.PrintPretty(&book)
+		}
+	}
+
+}
