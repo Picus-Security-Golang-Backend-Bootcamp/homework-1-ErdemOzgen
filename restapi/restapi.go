338dff237954fa9c0a102e3cde17d95143048f0e
ErdemOzgen <m.erdemozgen@gmail.com>
2022-02-23 23:24:44 +0300

    add GetBook method

--- restapi/restapi.go
+++ restapi/restapi.go
@@ -1,7 +1,28 @@
 package restapi
 
-import "homework-1-ErdemOzgen/model"
+import (
+	"encoding/json"
+	"fmt"
+	"homework-1-ErdemOzgen/model"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Book model.Book
 
 var books []Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, b := range books {
+		//fmt.Println(params["author"])
+		if b.Author == params["author"] {
+			fmt.Println(b.Author)
+			json.NewEncoder(w).Encode(b)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("Book not found")
+}
