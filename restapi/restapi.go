f6999d157b6d54f50b4bd7fa1dfb5f55d6997aef
ErdemOzgen <m.erdemozgen@gmail.com>
2022-02-23 23:37:08 +0300

    Create new function for searching for title

--- restapi/restapi.go
+++ restapi/restapi.go
@@ -16,7 +16,7 @@ var books []Book
 func GetBookByAuthor(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for _, b := range books {
-		//fmt.Println(params["author"])
+
 		if b.Author == params["author"] {
 			fmt.Println(b.Author)
 			json.NewEncoder(w).Encode(b)
@@ -24,7 +24,21 @@ func GetBookByAuthor(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	json.NewEncoder(w).Encode("Book not found")
+	json.NewEncoder(w).Encode("Book not found-> Searched for author")
+}
+
+func GetBookByTitle(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, b := range books {
+
+		if b.Author == params["title"] {
+			fmt.Println(b.Author)
+			json.NewEncoder(w).Encode(b)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("Book not found-> Searched for title")
 }
 
 func GetAllBooks(w http.ResponseWriter, r *http.Request) {
