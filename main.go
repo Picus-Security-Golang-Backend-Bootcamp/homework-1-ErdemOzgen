7141c04672c5dd3cc5c3146d11c117c14bd4f49d
ErdemOzgen <m.erdemozgen@gmail.com>
2022-02-23 11:42:32 +0300

    create package for json ops

--- main.go
+++ main.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
-	"os"
+	"homework-1-ErdemOzgen/jsonops"
 )
-
-func OpenJsonFile() {
-	jsonFile, err := os.Open("test.json")
-
-	// if we os.Open returns an error then handle it
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer jsonFile.Close()
-	fmt.Println("Successfully Opened users.json")
-	var buf bytes.Buffer
-	io.Copy(&buf, jsonFile)
-	asString := buf.String()
-	fmt.Println(asString)
-
-}
 
 func main() {
 	fmt.Println("Staring Program")
-	OpenJsonFile()
+	jsonops.OpenJsonFile()
 }
