b4dbc88db7ee6574260d289b72ea59a5408ad7b0
Erdem Ozgen <m.erdemozgen@gmail.com>
2022-02-25 14:31:09 +0300

    add list functions and remove unwanted comments

--- cmd/searchA.go
+++ cmd/searchA.go
@@ -43,13 +43,4 @@ var searchACmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(searchACmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// searchACmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// searchACmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/searchT.go
+++ cmd/searchT.go
@@ -44,13 +44,4 @@ var searchTCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(searchTCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// searchTCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// searchTCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
