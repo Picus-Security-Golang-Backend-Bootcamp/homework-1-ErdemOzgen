7fb47838fc05073dc434431b19aca2a70ce53a2b
Erdem Ozgen <m.erdemozgen@gmail.com>
2022-02-25 15:56:02 +0300

    fix found not found functions

--- search/search.go
+++ search/search.go
@@ -9,30 +9,37 @@ import (
 type Book model.Book
 
 func SearchByAuthor(author string, books []Book) {
+	counter := 0
 	fmt.Println("Searching...") // TODO: After location set get rid of this line
-	for i, book := range books {
+	for _, book := range books {
 		if book.Author == author {
-			fmt.Println("Founded") // TODO: After location set get rid of this line
+			counter++
+			fmt.Println("FOUNDED Count :", counter) // TODO: After location set get rid of this line
 			//fmt.Println(book)
 			utils.PrintPretty(&book)
 		}
-		if i == len(books)-1 && book.Author != author {
-			fmt.Println("Not found") // TODO: After location set get rid of this line
-		}
+
+	}
+	if counter == 0 {
+		fmt.Println("NOT FOUND") // TODO: After location set
+		counter = 0              // Not needed for now but will be useful later
 	}
 
 }
 func SearchByTitle(title string, books []Book) {
+	counter := 0
 	fmt.Println("Searching...") // TODO: After location set get rid of this line
-	for i, book := range books {
+	for _, book := range books {
 		if book.Title == title {
-			fmt.Println("Founded") // TODO: After location set get rid of this line
+			counter++
+			fmt.Println("FOUNDED Count :", counter) // TODO: After location set get rid of this line
 			//fmt.Println(book)
 			utils.PrintPretty(&book)
 		}
-		if i == len(books)-1 && book.Title != title {
-			fmt.Println("Not found") // TODO: After location set get rid of this line
-		}
+	}
+	if counter == 0 {
+		fmt.Println("NOT FOUND") // TODO: After location set
+		counter = 0              // Not needed for now but will be useful later
 	}
 }
 func ListBooks(books []Book) {
